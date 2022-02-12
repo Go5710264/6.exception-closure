5566c1953b6b465ea4034c80411f437f696929a3
Dima Shirkov <dimashirkov@list.ru>
2022-02-12 23:26:39 +0300

    Dak1 draft v1

---
+++ main.js
@@ -0,0 +1,14 @@
+// Задача 1
+
+// console.log(validateCount("115px"));
+// console.log(validateCount("px115"));
+// console.log(validateCount(NaN));
+// console.log(validateCount(404));
+
+// Задача 2
+
+let newTriangle = new Triangle(5, 6, 7);
+console.log(newTriangle.getPerimeter());
+console.log(newTriangle.getArea());
+getTriangle(10, 15, 20);
+getTriangle(10, 9, 1);
---
+++ task.js
@@ -0,0 +1,47 @@
+// Задача 1
+
+// function parseCount(parse) {
+//     let numberPerLine = Number.parseInt(parse);
+//     if(Number.isNaN(numberPerLine)) {
+//         throw new Error('Невалидное значение');
+//     }
+//     return numberPerLine;
+// }
+
+// function validateCount(parse) {
+//     try {
+//         return parseCount(parse);
+//     } catch (err) {
+//         return err;
+//     }
+// }
+
+// Задача 2
+
+class Triangle{
+    constructor(sideA, sideB, sideC){
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        if ((this.sideA + this.sideB) < this.sideC || (this.sideB + this.sideC) < this.sideA || (this.sideC + this.sideA) < this.sideB) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+    }
+
+    getPerimeter(){
+        return this.sideA + this.sideB + this.sideC;
+    }
+
+    getArea(){
+        let semiPerimeter = this.getPerimeter() / 2;
+        let area = Math.sqrt(semiPerimeter * (semiPerimeter - this.sideA) * (semiPerimeter - this.sideB) * (semiPerimeter - this.sideC));   
+        return area.toFixed(3);
+    }
+}
+function getTriangle(sideA, sideB, sideC) {
+    try {
+        return new Triangle(sideA, sideB, sideC);
+    } catch (err) {
+        return [getArea(), getPerimeter()]
+    }
+}
